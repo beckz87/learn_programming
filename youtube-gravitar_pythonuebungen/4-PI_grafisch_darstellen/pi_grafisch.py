a41a1030b8cc4e7916a2a63234d845c3f94f21af
beckz87 <beckz87@gmail.com>
2020-01-05 16:23:56 +0100

    pfad

--- youtube-gravitar_pythonuebungen/4-PI_grafisch_darstellen/pi_grafisch.py
+++ youtube-gravitar_pythonuebungen/4-PI_grafisch_darstellen/pi_grafisch.py
@@ -1,5 +1,5 @@
 import os
-os.chdir("\youtube-gravitar_pythonuebungen\4-PI_grafisch_darstellen\")
+os.chdir("youtube-gravitar_pythonuebungen\\4-PI_grafisch_darstellen")
 with open("pi_as_hell.txt", "r") as f:
     pi = f.read()
 print(pi[0:10])
