10933842a0b6071f17427ad170fdfe9460fda3d9
beckz87 <beckz87@gmail.com>
2020-01-05 15:09:23 +0100

    txt

--- YouTuber( Gravitar ) - Python-Übungen/4-PI grafisch darstellen/PI-grafisch.py
+++ YouTuber( Gravitar ) - Python-Übungen/4-PI grafisch darstellen/PI-grafisch.py
@@ -1,4 +1,25 @@
-with open("pi1000000.txt") as f:
-    pi = f.read()
+import turtle as tu
 
-print(len(pi))
+lines = 100_000
+
+with open("pi1000000.txt", "r") as f:
+  pi = f.read()
+
+tu.mode('logo')
+tu.tracer(False)
+tu.screensize(6000, 6000, 'black')
+tu.colormode(255)
+
+
+for n in range(lines):
+  color = int(n/(lines/255))
+  tu.pencolor(255, 255-color, color)
+  zahl = int(pi[n])
+  rotation = zahl * 36
+  tu.setheading(rotation)
+  tu.forward(3)
+  if n % 10_000 == 0:
+    tu.update()
+
+tu.getcanvas().postscript(file='PI_Picture.ps')
+tu.done()
