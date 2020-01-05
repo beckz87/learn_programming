96661ef250bc40040a386768d432aec0d2234076
beckz87 <beckz87@gmail.com>
2020-01-05 07:52:33 +0100

    jo

---
+++ spam.py
@@ -0,0 +1,10 @@
+
+
+
+for strophe in range(5):
+    for n in range(2):
+        for i in range(4):
+            print("SPAM ",end='')
+        print()
+    print("WOUNDERFUL SPAM, BEAUTIFUL SPAM")
+    print()
