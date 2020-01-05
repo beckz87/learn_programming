d70c0b5d4824ba300e174c9cbb3d99b44449abc2
beckz87 <beckz87@gmail.com>
2020-01-05 08:08:34 +0100

    g

--- spam.py
+++ spam.py
@@ -1,10 +1,14 @@
+from random import shuffle
 
-
+liste = "amazing gorgeous blazing stunning stunning greatest best fantastic phenomenal \
+    delightful ambitious outstanding incredible spectacular super cool magical \
+        revolutionary beautiful jaw-dropping".upper().split()
+shuffle(liste)
 
 for strophe in range(5):
     for n in range(2):
         for i in range(4):
             print("SPAM ",end='')
         print()
-    print("WOUNDERFUL SPAM, BEAUTIFUL SPAM")
+    print(liste.pop() +" SPAM, " + liste.pop()+" SPAM")
     print()
