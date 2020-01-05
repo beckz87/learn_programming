1887067c8a7f12cb0a998b3ad42ce5621cb54171
beckz87 <beckz87@gmail.com>
2020-01-05 08:31:50 +0100

    l

--- spam.py
+++ spam.py
@@ -8,7 +8,10 @@ shuffle(liste)
 for strophe in range(5):
     for n in range(2):
         for i in range(4):
-            print("SPAM ",end='')
+            print("SPAM ", end='')
         print()
-    print(liste.pop() +" SPAM, " + liste.pop()+" SPAM")
+    el1 = liste.pop()
+    el2 = liste.pop()
+
+    print("{} SPAM, {} SPAM".format(el1, el2))
     print()
