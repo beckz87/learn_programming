15e349ba1092cda50de30a3c927bd6d8d440a7bf
beckz87 <beckz87@gmail.com>
2020-01-05 10:57:08 +0100

    finish

--- Reiskörner-Schachbrett-zählung_dar-grafisch.py
+++ Reiskörner-Schachbrett-zählung_dar-grafisch.py
@@ -1,17 +1,27 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt  # matplotlib.pyplot geladen und als "plt" aufrufbar
 summe = 0
 feldListe = []
 
 for feld in range(64):
-    reiskorn = 2**feld
+    reiskorn = 2**feld  # Aus doppelt * wird hoch
     feldListe.append(reiskorn)
-    summe += reiskorn
+    summe += reiskorn  # Durch "+=" werden der bestehenden Summme weitere Körner hinzugefügt
     print(f"Feld {feld+1}. = {reiskorn:>30,} Reiskörner und damit insgesamt"
-          "{summe:>30,} Reiskörner")
+          f"{summe:>30,} Reiskörner")
 
 gewicht = summe * 0.02 / 1000 / 1000
 print()
 print("Wenn ein Reiskorn 0,02 Gramm wiegt, wiegen die gesamten"
       f" Reiskörner {gewicht:,.0f} Tonnen")
-plt.plot(feldListe)
+
+plt.title("Anzahl von Reiskörner auf jeweiligen Feldern")
+plt.xlabel("Anzahl der Schachfelder")
+plt.ylabel("Anzahl der Reiskörner [in Trillionen]")
+plt.grid(True)  # Hilfgitter ein-/ausblenden
+
+plt.yscale('log')  # Achsenausrichtung an X oder Y logharithmisch ausrichten
+# Syntax: plt.axis([xmin, xmax, ymin, ymax]) - eigene Skalierung festlegen
+#plt.axis([0, 63, 0, 10**18])
+
+plt.plot(feldListe, color="#1abc9c")
 plt.show()
