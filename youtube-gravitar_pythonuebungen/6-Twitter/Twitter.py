b24e6619df986c9daddf4cf87fcc8d2a03f529d8
beckz87 <beckz87@gmail.com>
2020-01-05 20:15:40 +0100

    kkk

--- youtube-gravitar_pythonuebungen/6-Twitter/Twitter.py
+++ youtube-gravitar_pythonuebungen/6-Twitter/Twitter.py
@@ -10,23 +10,19 @@ os.chdir("youtube-gravitar_pythonuebungen\\6-Twitter")
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 
-with open("Teil_05_Alice_in_wonderland.txt") as f:
-    text = f.read()
-
 wordcloud = WordCloud(width=1920, height=1200)
-
-STOPWORDS.add('said')
-STOPWORDS.add('illustration')
-
-wordcloud.generate(text)
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.show()
-
 
 auth = tweepy.OAuthHandler(ConsumerKey, ConsumerSecret)
 api = tweepy.API(auth)
+text = ''
 
 tweeds = api.user_timeline(screen_name='Sunny_Tter',
                            count=10, include_rts=False, tweet_mode='extended')
 for tweed in tweeds:
-    print(tweed.full.text)
+    text = text + ' '+ tweed.full_text
+
+STOPWORDS.update(['http', 'co', 'https'])
+wordcloud.generate(text)
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
+plt.show()
