547ef7b760fb1009f49ce9fa1dcdcf93a0221baf
beckz87 <beckz87@gmail.com>
2020-01-05 16:53:05 +0100

    eee

--- youtube-gravitar_pythonuebungen/4-PI_grafisch_darstellen/pi_grafisch.py
+++ youtube-gravitar_pythonuebungen/4-PI_grafisch_darstellen/pi_grafisch.py
@@ -1,6 +1,19 @@
-import os
+import turtle as tu
+import os  # MS-Struktur laden
+# Wechsle ins Unterverzeichniss
 os.chdir("youtube-gravitar_pythonuebungen\\4-PI_grafisch_darstellen")
 with open("pi_as_hell.txt", "r") as f:
     pi = f.read()
-print(pi[0:10])
-print(pi[-10:])
+
+# print(pi[0:10])  # zeigt die ersten 10 zeichen
+# print(pi[-10:])  # zeigt die letzten 10 zeichen
+# print(len(pi))  # zählt alle zeichen
+
+lines = 100
+for n in range(lines):
+    zahl = int(pi[n])
+    rotation = zahl * 36
+    tu.setheading(rotation)
+    tu.forward(50)
+
+tu.done()
