a4fcd972b57245f3d61cfce65b673807696356f7
beckz87 <beckz87@gmail.com>
2020-01-05 09:27:57 +0100

    2

--- Reiskörner-Schachbrett-zählung.py
+++ Reiskörner-Schachbrett-zählung.py
@@ -2,9 +2,9 @@ summe = 0
 for feld in range(64):
     reiskorn = 2**feld
     summe = summe + reiskorn
-    print("Feld {}. = {:>30,} Reiskörner und damit insgesamt {:>30,} Reiskörner"
-          .format(feld+1, reiskorn, summe))
+    print("Feld {}. = {:>30,} Reiskörner und damit insgesamt" \
+        "{:>30,} Reiskörner".format(feld+1, reiskorn, summe))
 gewicht = summe * 0.02 / 1000 / 1000
 print()
-print("Wenn ein Reiskorn 0,02 Gramm wiegt, wiegen die gesamten")
-print("Reiskörner {:18,.0f} Tonnen".format(gewicht))
+print("Wenn ein Reiskorn 0,02 Gramm wiegt, wiegen die gesamten" \
+    " Reiskörner {:,.0f} Tonnen".format(gewicht))
