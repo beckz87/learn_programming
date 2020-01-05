2c0733fd8b174ee0252175f31358bb322f6de764
beckz87 <beckz87@gmail.com>
2020-01-05 18:39:35 +0100

    ssss

--- youtube-gravitar_pythonuebungen/5-WordCloud/WordCloud.py
+++ youtube-gravitar_pythonuebungen/5-WordCloud/WordCloud.py
@@ -1,14 +1,17 @@
 import os  # MS-Struktur laden
 # Wechsle ins Unterverzeichniss
-os.chdir("youtube-gravitar_pythonuebungen\\4-WordCloud")
+os.chdir("youtube-gravitar_pythonuebungen\\5-WordCloud")
 
-from wordcloud import WordCloud
+from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 
 with open("Teil_05_Alice_in_wonderland.txt") as f:
     text = f.read()
 
 wordcloud = WordCloud(width=1920, height=1200)
+
+STOPWORDS.add('said')
+STOPWORDS.add('illustration')
 
 wordcloud.generate(text)
 plt.imshow(wordcloud, interpolation='bilinear')
